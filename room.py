b87e10a412a5877967d1914c5fe42161895e51e4
GuoYJ0521 <qwe1474376@gmail.com>
2023-12-19 01:33:47 +0800

    Edit layout in room1 and room2
    Finish the objects layout in room2.
    Change the way to display door objects in room1.
    Add the way to show the image of tolivingroom.(Matbe there will a better way)
    Add some images.

--- room.py
+++ room.py
@@ -3,16 +3,16 @@ import os
 
 from wall import *
 
-living_room_1_image = pygame.transform.scale(pygame.image.load(f"image/living_room/living_room_1.png"), (GAME_WIDTH, GAME_HEIGHT))
-living_room_2_image = pygame.transform.scale(pygame.image.load(f"image/living_room/living_room_2.png"), (GAME_WIDTH, GAME_HEIGHT))
-living_room_3_image = pygame.transform.scale(pygame.image.load(f"image/living_room/living_room_3.png"), (GAME_WIDTH, GAME_HEIGHT))
+living_room_1_image = pygame.transform.scale(pygame.image.load(f"image/living_room/Wall/living_room_1.png"), (GAME_WIDTH, GAME_HEIGHT))
+living_room_2_image = pygame.transform.scale(pygame.image.load(f"image/living_room/Wall/living_room_2.png"), (GAME_WIDTH, GAME_HEIGHT))
+living_room_3_image = pygame.transform.scale(pygame.image.load(f"image/living_room/Wall/living_room_3.png"), (GAME_WIDTH, GAME_HEIGHT))
 bedroom_1_image = pygame.transform.scale(pygame.image.load(f"image/living_room/bedroom.png"), (GAME_WIDTH, GAME_HEIGHT))
 bedroom_2_image = pygame.transform.scale(pygame.image.load(f"image/living_room/bedroom.png"), (GAME_WIDTH, GAME_HEIGHT))
 bedroom_3_image = pygame.transform.scale(pygame.image.load(f"image/living_room/bedroom.png"), (GAME_WIDTH, GAME_HEIGHT))
 bedroom_4_image = pygame.transform.scale(pygame.image.load(f"image/living_room/bedroom.png"), (GAME_WIDTH, GAME_HEIGHT))
-study_1_image = pygame.transform.scale(pygame.image.load(f"image/study/W1.png"), (GAME_WIDTH, GAME_HEIGHT))
-study_2_image = pygame.transform.scale(pygame.image.load(f"image/study/W2.png"), (GAME_WIDTH, GAME_HEIGHT))
-study_3_image = pygame.transform.scale(pygame.image.load(f"image/study/W3.png"), (GAME_WIDTH, GAME_HEIGHT))
+study_1_image = pygame.transform.scale(pygame.image.load(f"image/study/Wall/study_1.png"), (GAME_WIDTH, GAME_HEIGHT))
+study_2_image = pygame.transform.scale(pygame.image.load(f"image/study/Wall/study_2.png"), (GAME_WIDTH, GAME_HEIGHT))
+study_3_image = pygame.transform.scale(pygame.image.load(f"image/study/Wall/study_3.png"), (GAME_WIDTH, GAME_HEIGHT))
 
 # 章節1的所有房間/牆面
 #yj
@@ -27,12 +27,12 @@ class LivingRoom:
 
         # 此房間在不同牆面有的物品/座標 (包括移動鍵)
         # 牆面1有這些東西
-        self.__object_1 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),DoorToExit(210, 315)]
+        self.__object_1 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),DoorToExit(GAME_X, GAME_Y)]
         # 牆面2有這些東西
-        self.__object_2 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),DoorToStudy(210, 315),
-                           DoorToBedRoom(710, 315),Clock(GAME_X, GAME_Y)]
+        self.__object_2 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),DoorToStudy(GAME_X, GAME_Y),
+                           DoorToBedRoom(GAME_X, GAME_Y),Clock(GAME_X, GAME_Y)]
         # 牆面3有這些東西
-        self.__object_3 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),DoorToKitchen(490, 315),
+        self.__object_3 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),DoorToKitchen(GAME_X, GAME_Y),
                            Tv(GAME_X, GAME_Y),Wife_Ch1(GAME_X, GAME_Y)]
 
         # 當前牆面/建立此房間的牆面
@@ -52,11 +52,11 @@ class Study:
 
         # 此房間在不同牆面有的物品/座標
         # 牆面1有這些東西
-        self.__object_1 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50), Desk(GAME_X, GAME_Y)]
+        self.__object_1 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),Desk(GAME_X, GAME_Y),Book(GAME_X, GAME_Y)]
         # 牆面2有這些東西
-        self.__object_2 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50)]
+        self.__object_2 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),BookShelf(GAME_X, GAME_Y),Globe(GAME_X, GAME_Y),GlobeTable(GAME_X, GAME_Y),Window(GAME_X, GAME_Y)]
         # 牆面3有這些東西
-        self.__object_3 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),DoorToLivingRoom(200, 300)]
+        self.__object_3 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(900, 50),DoorToLivingRoom(GAME_X, GAME_Y, 0),Chest(GAME_X, GAME_Y),DroppedPainting(GAME_X, GAME_Y)]
         # 當前牆面/建立此房間的牆面
         # 不同房間的牆面數會不一樣 要注意
         self.wall = {'1': Wall(self.bg_image, self.__object_1),
@@ -99,11 +99,11 @@ class Bedroom:
         # 牆面1有這些東西
         self.__object_1 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(255, 245)]
         # 牆面2有這些東西
-        self.__object_2 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(255, 245), Tv(420, 400)]
+        self.__object_2 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(255, 245),Tv(GAME_X, GAME_Y)]
         # 牆面3有這些東西
-        self.__object_3 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(255, 245),DoorToLivingRoom(200, 300)]
+        self.__object_3 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(255, 245),DoorToLivingRoom(GAME_X, GAME_Y, 0)]
         # 牆面4有這些東西
-        self.__object_4 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(255, 245), Tv(GAME_X, GAME_Y)]
+        self.__object_4 = [RightButton(WIN_WIDTH - 50,WIN_HEIGHT/2),LeftButton(20,WIN_HEIGHT/2),MenuButton(255, 245),Tv(GAME_X, GAME_Y)]
 
         # 當前牆面/建立此房間的牆面
         # 不同房間的牆面數會不一樣 要注意
